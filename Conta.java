578ae07371b660c357644724f22ab5c562da2a89
yasmin-30 <161765927+yasmin-30@users.noreply.github.com>
2025-10-07 20:11:19 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -4,6 +4,7 @@ public abstract class Conta {
 	private int numeroId;
 	private String cliente;
 	public double saldo;
+	protected String tipo;
 
 	public Conta(String cliente, int id, double saldo) {
 		this.cliente = cliente;
@@ -26,31 +27,26 @@ public abstract class Conta {
 	
 	//listagem de contas
 	public void infosConta(Conta conta) {
-		String nomeClasse;
-        if (conta instanceof ITributavel) {
-			nomeClasse = "Corrente";
-		} else {
-			nomeClasse = "Poupança";
-		}
 		
 		System.out.println("");
 		System.out.println("Cliente: " + this.cliente);
 		System.out.println("N° da conta: " + this.numeroId);
 		System.out.println("Saldo: " + this.saldo);
-		System.out.println("Tipo de conta: " + nomeClasse);		
+		System.out.println("Tipo de conta: " + this.tipo);		
 		System.out.println("----------------------------------------");
 	}
 	
 	//Manuseio do dinheiro da conta
-	public void depositar(double valor) {
+	public boolean depositar(double valor) {
 		if(valor>=0) {
 			saldo+=valor;
+			return true;
 		} else {
-			System.out.println("O valor informado é negativo e, por isso, não será depositado!");
+			return false;
 		}
 	}
 	
 	//SAQUE E TRANSFERENCIA ABSTRATOS
 	public abstract boolean sacar(double valor);
-	public abstract boolean transferir(double valor);
+	public abstract void transferir(Conta destino, double valor);
 }
